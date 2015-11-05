110ba2185c48461e8d4392e8e9e00470f5f9dab3
Mariusz Gorski <marius.gorski@gmail.com>
2015-11-05 22:50:20 +0100

    Make GPIO pin for dht22 reader configurable

--- src/main/java/gorskima/sensors/config/DHT22Config.java
+++ src/main/java/gorskima/sensors/config/DHT22Config.java
@@ -15,15 +15,22 @@ public class DHT22Config {
 	@Value("${dht22-path}")
 	private String path;
 
+	@Value("${dht22-gpio-pin}")
+	private String gpioPin;
+
 	@Bean
 	public Sensor getDHT22Sensor() {
-		DataProvider tempReader = new ExecDataProvider(path + " -t");
+		DataProvider tempReader = new ExecDataProvider(buildFullPath(path, gpioPin, "-t"));
 		MeasurementProvider tempProvider = new DefaultProvider(tempReader, Prefix.NONE, Unit.CELSIUS);
 
-		DataProvider humidityReader = new ExecDataProvider(path + " -u");
+		DataProvider humidityReader = new ExecDataProvider(buildFullPath(path, gpioPin, "-u"));
 		MeasurementProvider humidityProvider = new DefaultProvider(humidityReader, Prefix.NONE, Unit.PERCENT);
 
 		return new Sensor("dht22", Lists.newArrayList(tempProvider, humidityProvider));
+	}
+
+	private String buildFullPath(String path, String gpioPin, String flag) {
+		return String.format("%s -p %s %s", path, gpioPin, flag);
 	}
 
 }
